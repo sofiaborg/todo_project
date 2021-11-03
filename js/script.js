c5d5fda8521cea8758cc508d8d60f21eb6231711
Sofia Borg <fia.borg@hotmail.com>
2021-11-03 16:31:03 +0100

    punkttre påbörjad

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-// window.onload = function () {
-//   listStart();
-// };
+window.onload = function () {
+  todolist();
+};
 
 let listItem = [
   "tvätta",
@@ -15,8 +15,15 @@ let listItem = [
 let ulList = document.createElement("ul");
 for (i = 0; i < listItem.length; i++) {
   let li = document.createElement("li");
+  let completeBtn = document.createElement("button");
+  completeBtn.innerHTML = "klarmarkera";
   li.innerHTML = listItem[i];
+  li.appendChild(completeBtn);
   ulList.appendChild(li);
 }
 
 document.body.appendChild(ulList);
+
+function todolist() {
+  let completeList = document.createElement("ul");
+}
