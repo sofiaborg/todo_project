f8d9bdc71b1ae9495ec86da26a5c8d5e7e5de47d
Sofia Borg <fia.borg@hotmail.com>
2021-11-08 14:49:55 +0100

    boolean på g

--- js/script.js
+++ js/script.js
@@ -1,48 +1,47 @@
-window.onload = function () {};
-
-let listItem = [
-  "tvätta",
-  "städa",
-  "plugga",
-  "träna",
-  "meditera",
-  "handla",
-  "duscha",
-];
-
-let ulList = document.createElement("ul");
-let container = document.getElementById("task-container");
-for (i = 0; i < listItem.length; i++) {
-  let li = document.createElement("li");
-  li.innerHTML = listItem[i];
-
-  let completeBtn = document.createElement("input");
-  completeBtn.type = "checkbox";
-
-  li.appendChild(completeBtn);
-  ulList.appendChild(li);
-  container.appendChild(ulList);
-
-  li.addEventListener("click", handleclick);
-  function handleclick() {
-    li.className = "markcomplete";
+//skapar class innehållandes class constructor. Jag skriver ej ut DONE för den ska ej ändras förrän i en if-sats
+class Todos {
+  constructor(taskName) {
+    this.uppgift = taskName;
+    this.status = false;
   }
 }
 
-container.appendChild(ulList);
+//skapar class-vaiabler som blir object
+let taskOne = new Todos("tvätta");
+let taskTwo = new Todos("städa");
+let taskThree = new Todos("plugga");
+
+window.onload = function () {
+  printTodos();
+};
+
+let listItem = [taskOne, taskTwo, taskThree]; // lägger object i en array
+console.log(listItem);
+let ulList = document.createElement("ul");
+let container = document.getElementById("task-container");
+
+//en funktion för att skriva ut array
+function printTodos() {
+  for (i = 0; i < listItem.length; i++) {
+    let li = document.createElement("li");
+    li.innerHTML = listItem[i].uppgift; //hämta item och visa propetryn för KEY taskName
+
+    let completeBtn = document.createElement("input");
+    completeBtn.type = "checkbox";
+    li.appendChild(completeBtn);
+
+    completeBtn.addEventListener("click", btnClick);
+    function btnClick() {
+      if (listItem[i].status === false) {
+        li.className = "markcomplete";
+        listItem[i].status = true;
+        console.log(listItem);
+      }
+    }
+
+    ulList.appendChild(li);
+  } //for-loop slutar
+  container.appendChild(ulList);
+}
+
 console.log(ulList);
-
-// let btnText = document.createTextNode("");
-//   completeBtn.classList.add("fas", "fa-check");
-// completeBtn.appendChild(btnText);
-
-//använda array push för att lägga till ny sak sist i en array
-
-// let todoInput = document.getElementById(todoInput);
-// let todoBtn = document.getElementById(todoBtn);
-// todoBtn.addEventListener("click", handleclickadd);
-// function handleclickadd() {
-//   let addedItems = document.createElement("li");
-//   addedItems.innerHTML = todoInput.value;
-//   ulList.appendChild(addedItems);
-// }
