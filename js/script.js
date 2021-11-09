4e182e81018053900c77fc4c49fc56319869072a
Sofia Borg <fia.borg@hotmail.com>
2021-11-09 16:30:31 +0100

    g ready

--- js/script.js
+++ js/script.js
@@ -15,33 +15,57 @@ window.onload = function () {
   printTodos();
 };
 
-let listItem = [taskOne, taskTwo, taskThree]; // lägger object i en array
-console.log(listItem);
+let listItem = [taskOne, taskTwo, taskThree];
 let ulList = document.createElement("ul");
 let container = document.getElementById("task-container");
 
 //en funktion för att skriva ut array
 function printTodos() {
-  for (i = 0; i < listItem.length; i++) {
+  ulList.innerHTML = ""; //gör innehållet tomt innan loopen för att inte allt ska skrivas ut dubbelt
+  for (let i = 0; i < listItem.length; i++) {
     let li = document.createElement("li");
-    li.innerHTML = listItem[i].uppgift; //hämta item och visa propetryn för KEY taskName
-
+    li.innerHTML = listItem[i].uppgift;
     let completeBtn = document.createElement("input");
     completeBtn.type = "checkbox";
+    completeBtn.className = "checkboxStyle";
     li.appendChild(completeBtn);
 
-    completeBtn.addEventListener("click", btnClick);
-    function btnClick() {
-      if (listItem[i].status === false) {
-        li.className = "markcomplete";
-        listItem[i].status = true;
-        console.log(listItem);
-      }
+    if (listItem[i].status == true) {
+      completeBtn.checked = true;
+      li.className = "markcomplete";
     }
+
+    completeBtn.addEventListener("click", () => {
+      btnClick(i);
+    });
 
     ulList.appendChild(li);
   } //for-loop slutar
   container.appendChild(ulList);
+  console.log(listItem);
+}
+
+function btnClick(i) {
+  //let id = this.id.split("-").pop();
+  console.log(i);
+
+  listItem[i].status = !listItem[i].status;
+  if (listItem[i].status == true) {
+    this.checked = true;
+  }
+
+  // detta är samma sak som att göra en if/else-sats med true/false
+  // if (listItem[i].status === false) {
+  //   listItem[id].status = true;
+  //   console.log(this);
+
+  //   console.log(listItem);
+  // } else {
+  //   listItem[id].status = false;
+  //   console.log(listItem);
+  // }
+
+  printTodos();
 }
 
 console.log(ulList);
